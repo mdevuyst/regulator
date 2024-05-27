7495bb11aec9d5e4122a487b8b3a809c8ce0931f
Matt DeVuyst <mdevuyst@gmail.com>
2024-05-26 19:45:25 -0700

    Cleaned up logs

--- src/main.rs
+++ src/main.rs
@@ -86,9 +86,11 @@ async fn main() -> Result<()> {
     let settings = Settings::parse();
     let listener = TcpListener::bind(&settings.listen).await?;
     let mut session_counter = 0;
+    debug!("Listening on {}", settings.listen);
     loop {
         let (socket, _) = listener.accept().await?;
         session_counter += 1;
+        debug!("Accepted connection. Starting session {}", session_counter);
         let settings_clone = settings.clone();
         tokio::spawn(async move {
             let res = handle_session(socket, session_counter, settings_clone).await;
@@ -105,10 +107,16 @@ async fn handle_session(
     settings: Settings,
 ) -> Result<()> {
     let now = std::time::Instant::now();
+    let pre = format!("[Session {session_number}] ");
+    debug!("{pre} Connecting to {}", settings.connect_to);
     let up_socket = TcpStream::connect(&settings.connect_to).await?;
+    debug!("{pre} Connected to {}", settings.connect_to);
 
     let (down_sock_rx, down_sock_tx) = down_socket.into_split();
     let (up_sock_rx, up_sock_tx) = up_socket.into_split();
+
+    // Create channels to pass buffers between the downstream reader to the upstream writer (for uploads).
+    // Then spawn the downstream reader and upstream writer.
 
     let (source, sink): (Sender<Option<BytesMut>>, Receiver<Option<BytesMut>>) =
         mpsc::channel(settings.upload_num_bufs);
@@ -136,6 +144,9 @@ async fn handle_session(
     };
     let upstream_tx = tokio::spawn(write(write_opts));
 
+    // Create channels to pass buffers between the upstream reader to the downstream writer (for downloads).
+    // Then spawn the upstream reader and downstream writer.
+
     let (source, sink): (Sender<Option<BytesMut>>, Receiver<Option<BytesMut>>) =
         mpsc::channel(settings.download_num_bufs);
     let (sink_return, source_return): (Sender<BytesMut>, Receiver<BytesMut>) =
@@ -161,6 +172,8 @@ async fn handle_session(
         rate: settings.upload_rate,
     };
     let downstream_tx = tokio::spawn(write(write_opts));
+
+    // Wait for all the tasks to complete and then print the session stats.
 
     let downstream_read_stats = downstream_rx.await?;
     let upstream_read_stats = upstream_rx.await?;
@@ -207,83 +220,71 @@ async fn handle_session(
 async fn read(mut opts: ReadOptions) -> ReadStats {
     let session_number = opts.session_number;
     let side = opts.side;
-    debug!("[Session {session_number}] {side:?} reader: starting");
+    let pre = format!("[Session {session_number}] {side:?} reader:");
+    debug!("{pre} starting");
     let mut buf_count = 0;
     let mut total_bufs_used = 0;
     let mut smallest_buffer_length = std::usize::MAX;
     let mut largest_buffer_length = 0;
     let mut total_bytes_read = 0;
     loop {
+        // Get a buffer to read into.  If we have not reached the maximum number of buffers, create
+        // a new one.  Otherwise, wait for a buffer to be returned from the writer.
         let mut buf: BytesMut = if buf_count < opts.num_bufs {
-            debug!(
-                "[Session {session_number}] {side:?} reader: creating buffer {} of {}",
-                buf_count + 1,
-                opts.num_bufs
-            );
+            debug!("{pre} creating buf {} of {}", buf_count + 1, opts.num_bufs);
             buf_count += 1;
             BytesMut::with_capacity(opts.buf_size)
         } else {
-            debug!("[Session {session_number}] {side:?} reader: waiting for return buffer");
+            debug!("{pre} waiting for return buffer");
             match opts.source_return.recv().await {
                 Some(mut buf) => {
-                    debug!("[Session {session_number}] {side:?} reader: got return buffer");
+                    debug!("{pre} got return buffer");
                     buf.clear();
                     buf
                 }
                 None => {
-                    debug!(
-                        "[Session {session_number}] {side:?} reader: no return buffer available"
-                    );
+                    debug!("{pre} no return buffer available");
                     break;
                 }
             }
         };
 
-        debug!("[Session {session_number}] {side:?} reader: Waiting for data to read...");
+        debug!("{pre} waiting for data to read...");
         match opts.sock_rx.read_buf(&mut buf).await {
             Ok(_) => {
                 total_bytes_read += buf.len();
                 if buf.is_empty() {
-                    // The peer has closed the connection.  Send a None to the
-                    // writer to signal that it should exit.  Then wait for it to exit (by waiting
-                    // for the mpsc channel to return an error, indicating that was closed).  If we
-                    // exit immediately, the mpsc channel will be immediately closed and the writer
+                    // The peer has closed the connection.  Send a None value to the
+                    // writer to signal that it should exit.  Then wait for the writer to exit (by
+                    // reading from the mpsc channel until it returns an error).  We should not just
+                    // exit immediately because the mpsc channel will be immediately closed and the writer
                     // may not have a chance to send all the buffers.
-                    debug!("[Session {session_number}] {side:?} reader: read 0 bytes");
+                    debug!("{pre} read 0 bytes, notifying the writer and waiting...");
                     if opts.source.send(None).await.is_err() {
-                        debug!(
-                            "[Session {session_number}] {side:?} reader: error passing buffer to other side"
-                        );
+                        debug!("{pre} error passing buf to other side");
                         break;
                     }
                     // Wait until the writer exits.
                     while opts.source_return.recv().await.is_some() {}
-                    debug!(
-                        "[Session {session_number}] {side:?} reader: writer has exited, exiting"
-                    );
+                    debug!("{pre} notified that the writer has exited");
                     break;
                 }
                 total_bufs_used += 1;
                 smallest_buffer_length = std::cmp::min(smallest_buffer_length, buf.len());
                 largest_buffer_length = std::cmp::max(largest_buffer_length, buf.len());
-                debug!(
-                    "[Session {session_number}] {side:?} reader: read {} bytes, passing buffer to other side",
-                    buf.len()
-                );
+                debug!("{pre} read {} bytes, passing buf to other side", buf.len());
                 if opts.source.send(Some(buf)).await.is_err() {
-                    debug!(
-                        "[Session {session_number}] {side:?} reader: error passing buffer to other side"
-                    );
+                    debug!("{pre} error passing buf to other side");
                     break;
                 }
             }
             Err(_) => {
-                debug!("[Session {session_number}] {side:?} reader: read error");
+                debug!("{pre} read error");
                 break;
             }
         }
     }
-    debug!("[Session {session_number}] {side:?} reader: exiting");
+    debug!("{pre} exiting");
     ReadStats {
         bytes_tranferred: total_bytes_read,
         buffers_used: total_bufs_used,
@@ -304,36 +305,37 @@ async fn read(mut opts: ReadOptions) -> ReadStats {
 async fn write(mut opts: WriteOptions) -> usize {
     let session_number = opts.session_number;
     let side = opts.side;
-    debug!("[Session {session_number}] {side:?} writer: starting and waiting for a buffer");
+    let pre = format!("[Session {session_number}] {side:?} writer:");
+    debug!("{pre} starting and waiting for a buffer");
     let mut total_bytes_written = 0;
     while let Some(buf) = opts.sink.recv().await {
         // The reader will send a None when it has finished reading.  If we get a None, we should
         // exit (dropping the mpsc channel, and causing the reader to exit as well).
         let Some(mut buf) = buf else {
-            debug!("[Session {session_number}] {side:?} writer: got None, exiting");
+            debug!("{pre} got notification that the reader has finished");
             break;
         };
         let buf_len = buf.len();
-        debug!("[Session {session_number}] {side:?} writer: got a buffer, writing...");
+        debug!("{pre} got a buffer of length {buf_len}, writing...");
         match opts.sock_tx.write_all_buf(&mut buf).await {
             Ok(_) => {
                 total_bytes_written += buf_len;
-                debug!("[Session {session_number}] {side:?} writer: write complete, clearing buffer and sending back");
+                debug!("{pre} write complete, clearing buffer and sending back");
                 buf.clear();
                 if opts.sink_return.send(buf).await.is_err() {
-                    debug!("[Session {session_number}] {side:?} writer: error returning buffer");
+                    debug!("{pre} error returning buffer");
                     break;
                 }
-                debug!("[Session {session_number}] {side:?} writer: buffer returned");
+                debug!("{pre} buffer returned");
             }
             Err(_) => {
                 total_bytes_written += buf.remaining();
-                debug!("[Session {session_number}] {side:?} writer: write error");
+                debug!("{pre} write error");
                 break;
             }
         }
-        debug!("[Session {session_number}] {side:?} writer: Waiting for another buffer");
+        debug!("{pre} waiting for another buffer");
     }
-    debug!("[Session {session_number}] {side:?} writer: exiting");
+    debug!("{pre} exiting");
     total_bytes_written
 }
